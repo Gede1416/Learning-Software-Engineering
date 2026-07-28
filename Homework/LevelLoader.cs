b2bcd666347a3685ea9a3cfa636d75bd1cf9cbe5
power_gede <3273415848@qq.com>
2026-07-29 01:09:44 +0800

    feat: 设计模式/模板方法-作业实现 2026-07-27

--- Homework/LevelLoader.cs
+++ Homework/LevelLoader.cs
@@ -65,6 +65,16 @@ namespace StudyNotes.Homework
         {
             Console.WriteLine("  播放 BGM: Forest_Theme.mp3");
         }
+
+        protected override bool HasCutscene() => true;
+
+        protected override void PlayCutscene()
+        {
+            base.PlayCutscene();
+            //播放视频
+        }
+
+
     }
 
     // ====== 地牢关卡（无过场）======
@@ -73,19 +83,24 @@ namespace StudyNotes.Homework
         protected override void LoadAssets()
         {
             // TODO
+            Console.WriteLine("  加载地牢场景资源: 牢房 楼梯 ...");
         }
 
         protected override void SetupEnemies()
         {
             // TODO
+            Console.WriteLine("  初始化敌人: 地牢守卫 ×5, 骷髅  ×3");
         }
 
         protected override void PlayBGM()
         {
             // TODO
+            Console.WriteLine("  播放 BGM: Dungeon_Theme.mp3");
         }
     }
 
     // ====== 思考题 ======
-    // ...
+    // ... 1. 直接修改管线 改动量大且违背 sop原则
+    //      2. 创建一个管线子类 给需要细化的步骤添加子方法来进行步骤扩展
+    //      3. 当需要扩展的方法过多 已经形成一个 庞大的 方法树时 使用 一个新的管线/组合模式进行抽离 
 }
